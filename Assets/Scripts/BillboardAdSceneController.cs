1b33006d22f734512fcd81af0b6dd56724569394
nuuneoi <sittiphol@gmail.com>
2018-11-13 05:03:08 +0700

    Add Billboard Ad Code Example

---
+++ Assets/Scripts/BillboardAdSceneController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using OmniVirt;
+
+public class BillboardAdSceneController : MonoBehaviour {
+
+    public GameObject adPlaneHorizontal;
+    public GameObject adPlaneVertical;
+
+    BillboardAd billboardAdHorizontal;
+    BillboardAd billboardAdVertical;
+
+	// Use this for initialization
+	void Start () {
+        billboardAdHorizontal = new BillboardAd(1557);
+        billboardAdHorizontal.LoadAd(adPlaneHorizontal);
+
+        billboardAdVertical = new BillboardAd(1556);
+        billboardAdVertical.LoadAd(adPlaneVertical);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+    }
+}
